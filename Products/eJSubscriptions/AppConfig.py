8e0d1dfdae081481b93caeaa24ec840a79a8d7f4
Hendrik Bunke <h.bunke@zbw.eu>
2010-11-01 14:12:39 +0100

    started refactoring

---
+++ Products/eJSubscriptions/AppConfig.py
@@ -0,0 +1,7 @@
+VALID              = "Your e-mail has been added. Shortly you will get an activation e-mail. Please follow its instructions to activate your subscription."
+NOT_VALID          = "Sorry, this is not a valid e-mail address."
+ALREADY_REGISTERED = "Your e-mail address has already been registered."
+
+ACTIVATED          = "Your e-mail address has been activated"
+ALREADY_ACTIVATED  = "Your e-mail address has already been activated"
+DELETED            = "Your e-mail address has been deleted"
